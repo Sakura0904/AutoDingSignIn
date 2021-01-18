4bcdf0c62c9fc217a1084f044d5fae8aeb02d8c9
Sakura <52352232+Sakura0904@users.noreply.github.com>
2021-01-19 02:05:53 +0800

    Add files via upload

--- AutoDingSignIn/AutoDIngSignIn_V1.3.0.js
+++ AutoDingSignIn/AutoDIngSignIn_V1.3.0.js
@@ -5,7 +5,7 @@ ui.layout(
         <vertical padding="25">
             <text gravity="left" textSize="23sp" textColor="blue" textStyle="bold"> 浙江纺织服装职业技术学院</text>
             <text gravity="right" textSize="23sp" textColor="blue" textStyle="bold"> 钉钉体温自动签到</text>
-            <text gravity="right" textSize="10sp" textColor="black" textStyle="bold"> 版本：V1.1.2</text>
+            <text gravity="right" textSize="10sp" textColor="black" textStyle="bold"> 版本：V1.3.0</text>
             <vertical>
                 <text textColor="white" textSize="17sp" bg="#00ccff"  text="提示: "/>
                 <text textColor="red" text="1.如果您在使用过程中出现身体体温不正常的情况，请立刻停止使用本软件，积极配合学校的体温测量工作！"/>
@@ -13,54 +13,52 @@ ui.layout(
                 <text textColor="red" text="3.请将软件的'无障碍服务'等功能打开，否则将手机将出现无法自动签到"/>
                 <text text="4.本软件不会进行联网操作，即不会泄露您任何隐私。"/>
                 <button id="userHalp"  bg="#ffc0cd"   text="如何使用本软件？"/>
+                <text text=""/>
                 <text gravity="center" textSize="20sp" text="( ･´ω`･ )亲，请放心使用~"/>
                 <text text=""/>
             </vertical>
 
             <text textColor="white" textSize="17sp"  bg="#00ccff" text="任务时间："/>
+            
             <vertical>
                 <horizontal>
                     <checkbox  id="uesrClickMorning" checked="true" text="早间打卡"></checkbox>
                     <input id="timeMorningHour" padding="10" gravity="center" text="7" inputType="number"/>
                     <text text=":"/>
-                    <input id="timeMorningMin" padding="10" gravity="center" text="10" inputType="number"/>
+                    <input id="timeMorningMin" padding="10" gravity="center" text="12" inputType="number"/>
                     <text text="推荐7:10"/>
                 </horizontal>
                 <horizontal>
                     <checkbox  id="uesrClickNoon"  text="午间打卡"></checkbox>
                     <input id="timeNoonHour" padding="10" gravity="center" text="12" inputType="number"/>
                     <text text=":"/>
-                    <input id="timeNoonMin" padding="10" gravity="center"  text="10" inputType="number"/>
+                    <input id="timeNoonMin" padding="10" gravity="center"  text="13" inputType="number"/>
                     <text text="推荐12:10"/>
                 </horizontal>
                 <horizontal>
                     <checkbox  id="uesrClickEvening" checked="true" text="晚间打卡"></checkbox>
                     <input id="timeEveningHour" padding="10" gravity="center" text="21" inputType="number"/>
                     <text text=":"/>
-                    <input id="timeEveningMin" padding="10" gravity="center" text="10" inputType="number"/>
+                    <input id="timeEveningMin" padding="10" gravity="center" text="14" inputType="number"/>
                     <text text="推荐21:10"/>
                 </horizontal>
                 <text text=""/>
             </vertical>
-
+            
                 <text textColor="white" textSize="17sp"  bg="#00ccff" text="开机密码："/>
                 <text text="用于自动解锁您的手机"/>
             <horizontal>
-                <radiogroup>
-                    <radio id="keyFore" text="4位密码"></radio>
-                    <radio id="keySix"  text="6位密码"></radio>
-                    <radio id="keyVoid" text="无密码" checked="true"></radio>
-                </radiogroup>
+                <button id="userPhoneKey"  bg="#836FFF" w="*"  text="开机密码设置"/>
             </horizontal>
             <text text=""/>
             <checkbox id="agrre" textSize="17sp" textColor="red"    text="同意上述提示,既可使用本软件"></checkbox>
             <text text=""/>
 
+            <button     id="userButtonStart" w="*"   h="60" bg="#ffc0cd" text="启动定时打卡程序"/>
             <button     id="userButtonOne" w="*"   h="60"  text="一键打卡"/>
-            <button     id="userButtonStart" w="*"   h="60"  text="启动定时打卡程序"/>
             <horizontal>
                 <button id="userButtonStop" w="auto"   text="停止脚本"/>
-                <button id="userButtonTest" w="*"   text="测试打卡程序   (点击后手动请息屏)"/>
+                <button id="userButtonTest" w="*"   text="测试解锁程序  (点击后请手动息屏)"/>
             </horizontal>
 
             <text text=""/>
@@ -72,27 +70,26 @@ ui.layout(
             <text text=""/>
 
             <vertical>
-                <button id="userUpDate" w="*" bg="#FFE1FF"  text="更新到最新版本"/>
+                <button id="userUpDate" w="*" bg="#54FF9F"  text="蒲公英更新版本"/>
+                <button id="userGitHub" w="*" bg="#000000" textColor="#FFFFFF" text="GitHub开源"/>
                 <text text=""/>
                 <text text="若您觉得本软件使用方便，不妨点击上方“打赏”按钮，"/>
                 <text text="(づ￣3￣)づ╭❤～给这个快吃不起饭的Up打点赏钱"/>
                 <text text=""/>
-                <img src="https://i2.hdslb.com/bfs/face/e9e59c290616df86e9d6e52211140a8fb958b3b0.jpg@150w_150h.jpg"/>
-                <text gravity="center" text="The Author:  樱落樱花瓣"/>
+                <img src="https://i0.hdslb.com/bfs/face/d3980a444270007643d11a7f950ee004ccd33df3.jpg@150w_150h.jpg"/>
+                <text gravity="center" text="The Author:  樱落の樱花瓣"/>
             </vertical>
-            
         </vertical>
     </scroll>
 );
 /***********************************UI用户定制页面*********************************/
 /***********************************全局变量*********************************/
-var phoneKey=0;
+var storage = storages.create("userPhoneKey");
 var userUi = {
     singinMorning:  1,
     singinNoon:     0,
     singinEvening:  1,
     agrre:0,
-    keynum:0,
 }
 var userTime={
     timeMorningHour:0,
@@ -146,34 +143,31 @@ threads.start(function(){
 })
     /*******************************同意选框****************************************/
     /*******************************密码选框****************************************/
-ui.keyFore.on("check",function(check){
+ui.userPhoneKey.click(function(){
     threads.start(function(){
-        if(check){
-            userUi.keynum=4;
-            phoneKey = dialogs.rawInput("请输入手机密码:").split("");
-            log("User:密码位数:"+userUi.keynum+" 密码："+phoneKey[0]+phoneKey[1]+phoneKey[2]+phoneKey[3]);
-            toast("您设置的密码位数为"+userUi.keynum+"位"+" 密码："+phoneKey[0]+phoneKey[1]+phoneKey[2]+phoneKey[3]);      
-        }    
-    })
-});
-ui.keySix.on("check",function(check){
-    threads.start(function(){
-        if(check){
-            userUi.keynum=6;
-            phoneKey = dialogs.rawInput("请输入手机密码:").split("");
-            log("User:密码位数:"+userUi.keynum+" 密码："+phoneKey[0]+phoneKey[1]+phoneKey[2]+phoneKey[3]+phoneKey[4]+phoneKey[5]);
-            toast("您设置的密码位数为"+userUi.keynum+"位"+" 密码："+phoneKey[0]+phoneKey[1]+phoneKey[2]+phoneKey[3]+phoneKey[4]+phoneKey[5]);      
-        }
-    })
+        var phoneKeyNum = dialogs.select("请选择秘密位数", ["无密码","四位密码", "六位密码" ]);
 
-});
-ui.keyVoid.on("check",function(check){
-    threads.start(function(){
-        if(check){
-            userUi.keynum=0;
-            log("User:密码位数"+userUi.keynum);
+        if(phoneKeyNum==0){
+            toast("您的手机无密码");
+            log("User:Key:null");
         }
-    })
+        else{
+            if(phoneKeyNum==1){
+                storage.put("keynum", 4);
+            }
+            if(phoneKeyNum==2){
+                storage.put("keynum", 6);
+            }
+            log("Auto:storage keynum:" + storage.get("keynum"));
+
+            var phoneKey = dialogs.rawInput("请输入手机密码:");
+            log("User:Key:"+phoneKey);
+            toast("您的手机密码是："+phoneKey);
+                
+            storage.put("key", phoneKey);
+            log("Auto:storage key:" + storage.get("key"));
+        }
+    });
 });
     /*******************************密码选框****************************************/
 
@@ -211,17 +205,13 @@ ui.userButtonTest.click(function(){//检查程序
     threads.shutDownAll();
     threads.start(function(){
         if(Check_the_ui()){
-            if(userUi.keynum==0){
-                toast("您手机的未设置解锁密码");
-                toast("软件将自动跳过自动解锁程序");
-            }
             if(confirm("是否开始签到功能的测试")){//对话框确认开启
                 alert("开启测试：请手动熄灭屏幕,测试程序5s倒计时");
                 sleep(5000);
             }
         }
-        log("User：主动启动检查程序");
-        Sakura_Test();
+        log("User：主动启动解锁程序");
+        Sakura_Unlock();
     })
 });
 ui.userButtonStop.click(function(){
@@ -245,6 +235,10 @@ ui.userUpDate.click(function(){
     app.openUrl("https://www.pgyer.com/sakura_AutoDingSignIn");
     alert("o(╥﹏╥)o","Up目前实力不足，请各位亲们手动下载，非常感谢~同名代码以开源到Github，如有兴趣欢迎访问~");
 });
+ui.userGitHub.click(function(){
+    app.openUrl("https://github.com/Sakura0904/AutoDingSignIn");
+    alert("o(╥﹏╥)o","Up目前实力不足，请各位亲们手动下载，非常感谢~同名代码以开源到Github，如有兴趣欢迎访问~");
+});
 /***********************************UI界面按钮多线程命令*********************************/
 /***********************************主函数命令*********************************/
 function Sakura_MainTime(){
@@ -262,16 +256,18 @@ function Sakura_MainTime(){
             log("Auto：晚间签到时间到"+userTime.timeEveningHour+":"+userTime.timeEveningMin);
             Check_the_screen();
         }
-        log("Procedure：当前时间是"+curTime.getHours() + ":" + curTime.getMinutes() + ":" + curTime.getSeconds());
-        sleep(10000);//10s
+
+        if(curTime.getSeconds()==30){
+            log("Procedure：当前时间是"+curTime.getHours() + ":" + curTime.getMinutes() + ":" + curTime.getSeconds());
+            sleep(300);
+        }
     }
 }
-function Sakura_Test(){//测试程序 解锁+签到
-    Bright_Screen();
-    if(userUi.keynum){
-        Unlock_Screen();
+function Sakura_Unlock(){//测试程序
+    Bright_Screen();//亮屏
+    if(storage.get("keynum")){
+        Unlock_Screen();//解锁
     }
-    Sakura_Ding();
 }
 function Sakura_Ding(){//签到程序 签到
     device.keepScreenOn(); //保持亮屏
@@ -321,10 +317,11 @@ function Check_the_screen(){//屏幕检查程序
         device.vibrate(100);sleep(200);device.vibrate(300);sleep(300);device.vibrate(100);
         toast("钉钉体温签到时间已到，考虑到您手机屏幕正在使用。");
         toast("本次'自动签到'程序已经关闭，请您稍后完成体温签到，谢谢(づ￣3￣)づ");
-        return ;//跳出本次定时签到层序
+        return ;//跳出本次定时签到层序 
     }
     else{
-        Sakura_Test();
+        Sakura_Unlock();//
+        Sakura_Ding();
     }
 }
 /***********************************子函数命令*********************************/
@@ -346,10 +343,10 @@ function User_DataSetIn(){
 //1.亮屏
 function Bright_Screen() {
     log("Procedure：进入'手机亮屏'子程序")
+    sleep(500);click(0);sleep(400);click(0);sleep(500);//模拟双击亮屏
     device.wakeUp()
     sleep(500);
     device.wakeUpIfNeeded(); //唤醒设备
-    sleep(500);click(0);sleep(400);click(0);sleep(500);//模拟双击亮屏
     device.keepScreenOn(); //保持亮屏
     if (!device.isScreenOn()) {
         log("Procedure：未唤醒");
@@ -374,12 +371,13 @@ function Bright_Screen() {
 //2.解锁--安卓7.0及以上才能支持手势及坐标操作
 function Unlock_Screen() {
     log("Procedure：进入'手机解锁'子程序");   
-    if(userUi.keynum){
+    if(storage.get("keynum")){
+        var phoneKey=storage.get("key").split("")
         desc(phoneKey[0]).findOne().click();log("Procedure：已输入密码："+phoneKey[0]);
         desc(phoneKey[1]).findOne().click();log("Procedure：已输入密码："+phoneKey[1]);
         desc(phoneKey[2]).findOne().click();log("Procedure：已输入密码："+phoneKey[2]);
         desc(phoneKey[3]).findOne().click();log("Procedure：已输入密码："+phoneKey[3]);
-        if(userUi.keynum==6){
+        if(storage.get("keynum")==6){
             desc(phoneKey[4]).findOne().click();log("Procedure：已输入密码："+phoneKey[4]);
             desc(phoneKey[5]).findOne().click();log("Procedure：已输入密码："+phoneKey[5]);
         }
@@ -390,23 +388,25 @@ function Unlock_Screen() {
 }
 //3.结束钉钉进程，确保启动钉钉后进入主页面
 function Close_App() {
-    log("Procedure：进入'停止钉钉进程'子程序");
-    app.openAppSetting("com.alibaba.android.rimet"); //进入钉钉设置
-    text(app.getAppName("com.alibaba.android.rimet")).waitFor();
-    let is_sure = textMatches(/(强制.*|.*停止|结束|强行.*)/).clickable(true).findOne();
-    if (is_sure.enabled()) {
-        log("Procedure：检测到停止按键");
-        sleep(1000);
-        textMatches(/(强制.*|.*停止|结束*.|.*确定.*)/).clickable(true).findOne().click();
-        sleep(1000);
-        textMatches(/(强制.*|.*停止|结束|.*确定.*)/).clickable(true).findOne().click();
-        log("Procedure："+app.getAppName("com.alibaba.android.rimet") + "应用已被关闭");
-        sleep(500);
-        home();
-    } else {
-        log("Procedure："+app.getAppName("com.alibaba.android.rimet") + "应用不能被正常关闭,重试");
-        sleep(500);
-        home();
+    if(device.brand!="Xiaomi"){//读取手机品牌
+        log("Procedure：进入'停止钉钉进程'子程序");
+        app.openAppSetting("com.alibaba.android.rimet"); //进入钉钉设置
+        text(app.getAppName("com.alibaba.android.rimet")).waitFor();
+        let is_sure = textMatches(/(强制.*|.*停止|结束.*|强行.*)/).clickable(true).findOne();
+        if (is_sure.enabled()) {
+            log("Procedure：检测到停止按键");
+            sleep(1000);
+            textMatches(/(强制.*|.*停止|.*结束.*|.*确定.*)/).clickable(true).findOne().click();
+            sleep(1000);
+            textMatches(/(强制.*|.*停止|.*结束.*|.*确定.*)/).clickable(true).findOne().click();
+            log("Procedure："+app.getAppName("com.alibaba.android.rimet") + "应用已被关闭");
+            sleep(500);
+            home();
+        } else {
+            log("Procedure："+app.getAppName("com.alibaba.android.rimet") + "应用不能被正常关闭,重试");
+            sleep(500);
+            home();
+        }
     }
 }
 //4.开启钉钉进程
@@ -415,6 +415,7 @@ function Open_App() {
     app.launchApp("钉钉");
     app.launchPackage("com.alibaba.android.rimet");
     sleep(7000);
+    /*
     if (id("et_pwd_login").exists()) { //判定是否在登录页面
         var 手机号码 = id("et_phone_input").findOne();
         手机号码.setText("请输入登录名");
@@ -433,17 +434,19 @@ function Open_App() {
             is_login();
         }
     }
+    */
 }
 //5.进入签到页面
 function Goto_AppClickInPage() {
     log("Procedure：进入'考勤页面'")
     while(1){
-        if (null != textMatches("浙纺服").clickable(true).findOne(3000)) {
+        if (null != textMatches("浙纺服").clickable(false).findOne(3000)) {
             toast("Text find:浙纺服")
-            coordZhefangfu = textMatches(/(.*浙纺服.*)/).findOnce()
+            coordZhefangfu = textMatches(/(浙纺服)/).findOnce()
             log("Procedure：Text find:浙纺服")
             sleep(500);
             coordZhefangfu.click();
+            click(500,2250);//点击浙纺服
             sleep(6000);
             log("Procedure：进入'浙纺服'");
         }
@@ -456,10 +459,9 @@ function Goto_AppClickInPage() {
             sleep(6000);
             log("Procedure：进入'浙纺服'");
         }
-
         if (null != textMatches("智慧学工").clickable(true).findOne(3000)) {
             toast("Text find:智慧学工")
-            coordZhihuixuegong = textMatches(/(.*智慧学工.*)/).findOne()
+            coordZhihuixuegong = textMatches(/(智慧学工)/).findOne()
             log("Procedure：Text find:智慧学工")
             sleep(500)
             coordZhihuixuegong.click();
@@ -469,7 +471,7 @@ function Goto_AppClickInPage() {
 
         if (null != textMatches("签到")) {
             toast("Text find:每日签到")
-            coordMeiriqiandao = textMatches(/(.*每日签到.*)/).findOne()
+            coordMeiriqiandao = textMatches(/(每日签到)/).findOne()
             log("Procedure：Text find:每日签到")
             sleep(500)
             coordMeiriqiandao.click();
@@ -490,7 +492,7 @@ function Sign_in(){
     sleep(500);
     click(425+5,1684+5);//点击签到按钮
 
-    toast("'签到'已点击");
+    alert("签到成功，-(゜-゜)つロ乾杯~");
     log("Procedure：点击签到");
 }
 /***********************************子函数命令*********************************/
